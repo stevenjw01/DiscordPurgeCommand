e374c60bb310859b532244e89a7700376e00ef94
Steve Williams <stevenjw01@gmail.com>
2018-03-29 21:44:09 -0400

    Update deleteMessages.cs

--- deleteMessages.cs
+++ deleteMessages.cs
@@ -20,69 +20,24 @@ namespace IdiotBOT.Modules.Admin
     public class deleteMessages : ModuleBase
     {
         [Command("purge", RunMode = RunMode.Async)]
+        [Alias("d")]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
         public async Task purgeFuction(int amount)
         {
-            string channelID = Context.Channel.Id.ToString();
-            
 
-            if(Context.User.Id == USER ID HERE || Context.User.Id == USER ID HERE) // NO "" ONLY USER ID NUMBER BLANK
-                {
-                  
-                   var messages = await Context.Channel.GetMessagesAsync(amount + 1).Flatten();
-                   await Context.Channel.DeleteMessagesAsync(messages);               
+                await Context.Message.DeleteAsync();
                 
-                }
-                else
-                {
-                    await Context.Message.DeleteAsync();
-                    await ReplyAsync("***```You don't have access!```***");
-                    return;
-                }
+
+                   
+                 var messages = await Context.Channel.GetMessagesAsync(amount + 1).Flatten();
+                 await Context.Channel.DeleteMessagesAsync(messages);
+                   
 
             }
-            else
-            {
-                await Context.Message.DeleteAsync();
-                await ReplyAsync("***```Wrong Channel!```***");
-                return;
-            }
+
 
         }
-        [Command("bigpurge", RunMode = RunMode.Async)]
-        public async Task bigPurge()
-        {
-            string channelID = Context.Channel.Id.ToString();
-
-
-            if (channelID == "CHANNEL ID HERE")
-            {
-                await Context.Message.DeleteAsync();
-
-                if (Context.User.Id == USER ID HERE)
-                {
-
-                    
-                    var messages = await Context.Channel.GetMessagesAsync(101).Flatten();
-                    await Context.Channel.DeleteMessagesAsync(messages);
-
-                    
-                }
-                else
-                {
-                    await Context.Message.DeleteAsync();
-                    await ReplyAsync("***```You don't have access!```***");
-                    return;
-                }
-
-            }
-            else
-            {
-                await Context.Message.DeleteAsync();
-                await ReplyAsync("***```Wrong Channel!```***");
-                return;
-            }
-        }
-
 
     }
 }
